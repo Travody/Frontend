100c9b645ddcadd56ff8a0675eea7c4f98c8ba47
raunak-dev-edu <raunak_2101cb47@iitp.ac.in>
2025-12-28 02:32:35 +0530

    lastname required remove

--- src/app/auth/traveler/page.tsx
+++ src/app/auth/traveler/page.tsx
@@ -227,7 +227,6 @@ export default function TravelerAuthPage() {
                       onChange={(e) => setRegisterData(prev => ({ ...prev, lastName: e.target.value }))}
                       className="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-teal-500"
                       placeholder="Last name"
-                      required
                     />
                   </div>
                 </div>
